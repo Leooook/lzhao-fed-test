374afc40eedd504d195486255284b551beee9ce0
Leooook <leoooooo1995@gmail.com>
2021-03-15 15:47:44 +1100

    feat(header.jsx & app.css): complete header and its style

--- .history/src/components/header_20210315154015.jsx
+++ .history/src/components/header_20210315154015.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Header extends Component {
 	render() {
-		return <div className="header">Member Card Information</div>
+		return <div></div>
 	}
 }
 
