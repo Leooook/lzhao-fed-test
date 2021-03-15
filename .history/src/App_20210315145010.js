cf1ac32d62024bc60b7a2954632ee5823200a681
Leooook <leoooooo1995@gmail.com>
2021-03-15 14:51:38 +1100

    docs(components & app.css): create new files of header/feature/detail.jsx & App.css

--- .history/src/App_20210315145010.js
+++ .history/src/App_20210315145010.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css'
 import Header from './components/header.jsx'
 import Feature from './components/feature.jsx'
+import Detail from './components/detail.jsx'
 
 export default function App() {
 	return (
---
+++ .history/src/components/feature_20210315144928.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class feature extends Component {
+	render() {
+		return <div />
+	}
+}
+
+export default Feature
